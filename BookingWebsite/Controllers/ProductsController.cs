7b80034d2ccc6128571a9b6ef11a4f9d8b98f917
PavKmiec <pawelkmiec07@gmail.com>
2018-12-13 20:26:22 +0000

    Added ProductsControlled with Index Action method

---
+++ BookingWebsite/Controllers/ProductsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookingWebsite.Data;
+using BookingWebsite.Models;
+using BookingWebsite.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingWebsite.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // we need to access database
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public ProductsViewModel ProductsVM { get; set; }
+
+
+        // constructor
+        public ProductsController(ApplicationDbContext db)
+        {
+            _db = db;
+
+            // initialise ProductsViewModel
+            ProductsVM = new ProductsViewModel()
+            {
+                ProductTypes = _db.ProductTypes.ToList(),
+                Tags = _db.Tags.ToList(),
+                Products = new Products()
+            };
+
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+
+
+            var products = _db.Products.Include(m => m.ProductTypes).Include(m => m.Tags);
+            return View(await products.ToListAsync());
+        }
+    }
+}
