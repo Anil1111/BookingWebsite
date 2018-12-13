ed14f7a0dbd7de466bbb3f83498eba46d447838c
PavKmiec <pawelkmiec07@gmail.com>
2018-12-13 20:12:05 +0000

    Added ProductsViewModel

---
+++ BookingWebsite/Models/ViewModel/ProductsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingWebsite.Models.ViewModel
+{
+    public class ProductsViewModel
+    {
+        public Products Products { get; set; }
+
+        public IEnumerable<ProductTypes> ProductTypes { get; set; }
+        public IEnumerable<Tags> Tags { get; set; }
+
+
+
+    }
+}
